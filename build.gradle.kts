f63471ba3628995bb7633530e1c5b5d6a71a183a
Michael Taege <mictaege@googlemail.com>
2025-05-23 12:12:40 +0200

    Publish to Central Portal using JReleaser

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
+import org.jreleaser.model.Active
+import org.jreleaser.model.Signing
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.22"
     `java-gradle-plugin`
     `maven-publish`
     id("com.gradle.plugin-publish") version "1.2.1"
     signing
+    id("org.jreleaser") version "1.18.0"
 }
 
 group = "io.github.mictaege"
-version = "2024.2"
+version = "2025.1-rc1"
 
 gradlePlugin {
     website.set("https://github.com/mictaege/arete-gradle")
@@ -35,7 +39,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.junit.platform:junit-platform-launcher:1.10.2")
-    implementation("io.github.mictaege:arete:2024.2")
+    implementation("io.github.mictaege:arete:2025.1-rc1")
     implementation("org.fusesource.jansi:jansi:2.4.1")
     implementation("com.google.guava:guava:33.2.0-jre")
     implementation("org.freemarker:freemarker:2.3.32")
@@ -124,17 +128,42 @@ publishing {
     }
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-            credentials {
-                username = if (hasProperty("ossrhUsername")) property("ossrhUsername") as String else ""
-                password = if (hasProperty("ossrhPassword")) property("ossrhPassword") as String else ""
-            }
+            name = "staging"
+            url = uri(layout.buildDirectory.dir("staging").get().asFile.toURI())
         }
     }
 }
 
-signing {
-    sign(publishing.publications["mavenJava"])
+jreleaser {
+    project {
+        copyright.set("Michael Taege")
+        description.set("This repository contains the Arete Gradle Plugin.")
+    }
+    signing {
+        active.set(Active.ALWAYS)
+        armored.set(true)
+        checksums.set(true)
+        mode.set(Signing.Mode.FILE)
+        passphrase.set(if (hasProperty("centralPortalKeyPwd")) property("centralPortalKeyPwd") as String else "")
+        publicKey.set(if (hasProperty("centralPortalPublicKey")) property("centralPortalPublicKey") as String else "")
+        secretKey.set(if (hasProperty("centralPortalSecretKey")) property("centralPortalSecretKey") as String else "")
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active.set(Active.ALWAYS)
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    username.set(if (hasProperty("centralPortalUsr")) property("centralPortalUsr") as String else "")
+                    password.set(if (hasProperty("centralPortalPwd")) property("centralPortalPwd") as String else "")
+                    stagingRepository(layout.buildDirectory.dir("staging").get().asFile.path)
+                }
+            }
+        }
+    }
+    release {
+        github {
+            enabled.set(false)
+        }
+    }
 }
